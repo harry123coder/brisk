f50998f81dd26365e29f0068544f222fc54826dd
harry123coder <96204162+harry123coder@users.noreply.github.com>
2021-12-19 16:54:32 +0500

    Add files via upload

---
+++ carousel.js
@@ -0,0 +1,21 @@
+$('#myCarousel2').carousel({
+    interval: 1000
+})
+
+$('.carousel .ppp').each(function() {
+    var minPerSlide = 4;
+    var next = $(this).next();
+    if (!next.length) {
+        next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+
+    for (var i = 0; i < minPerSlide; i++) {
+        next = next.next();
+        if (!next.length) {
+            next = $(this).siblings(':first');
+        }
+
+        next.children(':first-child').clone().appendTo($(this));
+    }
+});
